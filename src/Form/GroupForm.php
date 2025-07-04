0a72186824cdb20113cb80086a624b51b4ef3264
GabrielSAPONARA <gabriel.saponara.pro@protonmail.com>
2025-07-04 10:44:56 +0200

    :sparkles: add crud to Figure and Group

---
+++ src/Form/GroupForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Figure;
+use App\Entity\Group;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GroupForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Group::class,
+        ]);
+    }
+}
