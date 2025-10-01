7087f0eb283087e07aeb6ea70bb0f1c6ab31604a
GabrielSAPONARA <gabriel.saponara.pro@protonmail.com>
2025-10-01 12:11:16 +0200

    :sparkles: add pages to manage user account

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -4,34 +4,38 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+class ChangePasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username', TextType::class,
-                [
-                    'attr' =>
-                    [
-                        "class" => "form-control",
-                        "placeholder" => "Username",
-                    ]
-                ])
-            ->add('email', EmailType::class,
+            ->add('password', PasswordType::class,
             [
                 'attr' =>
                 [
                     "class" => "form-control",
-                    "placeholder" => "Email",
-                ]
+                    "placeholder" => "**********",
+                ],
+                'mapped' => false,
             ])
+            ->add('newPassword', RepeatedType::class,
+            [
+                'type' => PasswordType::class,
+                'first_options'  => ['label' => 'New Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'invalid_message' => 'The password fields must match.',
+                'attr' =>
+                [
+                    "placeholder" => "***********",
+                ],
+                'mapped' => false,
+            ])
+
         ;
     }
 
