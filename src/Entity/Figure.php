931c0cb16a3306cb2ff2325d9c89291cf4429212
GabrielSAPONARA <gabriel.saponara.pro@protonmail.com>
2025-11-13 15:41:51 +0100

    :card_file_box: add some constraints in entities

--- src/Entity/Figure.php
+++ src/Entity/Figure.php
@@ -7,8 +7,19 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: FigureRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_NAME', fields: ['name'])]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_SLUG', fields: ['slug'])]
+#[UniqueEntity(
+    fields: ['name'],
+    message: 'This figure name already exists.'
+)]
+#[UniqueEntity(
+    fields: ['slug'],
+    message: 'This figure slug already exists.'
+)]
 class Figure
 {
     #[ORM\Id]
