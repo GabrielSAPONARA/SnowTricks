66f9ae9d1942e3d404f0d934f73c91ea13f1249a
GabrielSAPONARA <gabriel.saponara.pro@protonmail.com>
2025-12-09 16:35:45 +0100

    :lipstick: finish the registration page's ui

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,9 +5,11 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -21,8 +23,21 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('email')
+            ->add('username', TextType::class,
+            [
+                'attr' => [
+                    "class" => "form-control",
+                    "placeholder" => "Username",
+                ]
+            ])
+            ->add('email', EmailType::class,
+                [
+                    'attr' =>
+                        [
+                            "class" => "form-control",
+                            "placeholder" => "test.test@gmail.com",
+                        ]
+                ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -36,6 +51,7 @@ class RegistrationFormType extends AbstractType
                 'options' => [
                     'attr' => [
                         'autocomplete' => 'new-password',
+
                     ],
                 ],
                 'first_options' => [
@@ -53,9 +69,19 @@ class RegistrationFormType extends AbstractType
                         new NotCompromisedPassword(),
                     ],
                     'label' => 'Password',
+                    'attr' =>
+                        [
+                            "class" => "form-control",
+                            "placeholder" => "**************",
+                        ]
                 ],
                 'second_options' => [
                     'label' => 'Repeat Password',
+                    'attr' =>
+                        [
+                            "class" => "form-control",
+                            "placeholder" => "**************",
+                        ]
                 ],
                 'invalid_message' => 'The password fields must match.',
                 // Instead of being set onto the object directly,
