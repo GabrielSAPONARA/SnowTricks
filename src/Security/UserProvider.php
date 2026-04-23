d7576a34b60cb3755f48359eca593c3b7a97e963
GabrielSAPONARA <gabriel.saponara.pro@protonmail.com>
2026-04-23 14:59:42 +0200

    :rotating_light: fix PHPStan error
    fix Class App\Security\UserProvider implements generic interface
    
     Symfony\Component\Security\Core\User\UserProviderInterface but does not specify its types: TUser  missingType.generics

--- src/Security/UserProvider.php
+++ src/Security/UserProvider.php
@@ -2,38 +2,48 @@
 
 namespace App\Security;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
+/**
+ * @implements UserProviderInterface<User>
+ */
 class UserProvider implements UserProviderInterface
 {
     public function __construct(private readonly UserRepository $userRepository) {}
 
-    public function loadUserByIdentifier(string $identifier): UserInterface
+    public function loadUserByIdentifier(string $identifier): User
     {
         $user = $this->userRepository->findOneBy(['username' => $identifier]);
 
-        if (!$user) {
+        if (!$user)
+        {
             $user = $this->userRepository->findOneBy(['email' => $identifier]);
         }
 
-        if (!$user) {
-            throw new UserNotFoundException(sprintf('User "%s" not found.',
-                $identifier));
+        if (!$user)
+        {
+            throw new UserNotFoundException(sprintf('User "%s" not found.', $identifier));
         }
 
         return $user;
     }
 
-    public function refreshUser(UserInterface $user): UserInterface
+    public function refreshUser(UserInterface $user): User
     {
+        if (!$user instanceof User)
+        {
+            throw new \InvalidArgumentException('Unsupported user class.');
+        }
+
         return $user;
     }
 
     public function supportsClass(string $class): bool
     {
-        return $class === \App\Entity\User::class;
+        return $class === User::class || is_subclass_of($class, User::class);
     }
 }
